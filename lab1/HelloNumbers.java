9a696a06ea7b665459f5f2e826443afe266a7047
biu <864722075@qq.com>
2021-02-16 16:00:17 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+       int x = 0; 
+       int sum = 0; 
+       for(; x < 10; x += 1){
+            sum = sum + x;
+            System.out.print(sum + " ");
+          
+       }
+
+    }
+}
