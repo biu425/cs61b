32f58926d93dcff3cbfe23c4456006a679e557c8
biu425 <864722075@qq.com>
2021-04-20 00:27:28 +0800

    modified Deque

--- proj1b/Deque.java
+++ proj1b/Deque.java
@@ -1,10 +1,7 @@
 public interface Deque<Item> {
-    void addFirst(Item item);
     void addLast(Item item);
     int size();
     void printDeque();
     Item removeFirst();
     Item removeLast();
-    Item get(int index);
-
 }
