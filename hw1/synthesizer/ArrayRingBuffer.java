e4e597f21b04da7d03b98bdf4b473b6b20ceb079
biu425 <864722075@qq.com>
2021-04-30 00:49:30 +0800

    finished HW1 with exception check

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -68,6 +68,9 @@ public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> {
     @Override
     public T peek() {
         //Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
         return rb[first];
     }
 
