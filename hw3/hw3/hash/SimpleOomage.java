354342e4a19f32f3797a9db9c9780f2ded944291
biu425 <864722075@qq.com>
2021-05-14 02:31:15 +0800

    finished HW3

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -10,26 +10,39 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
-        return false;
+        if (o == null) {
+            return false;
+        }
+        if (o == this){
+            return true;
+        }
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+        SimpleOomage other = (SimpleOomage) o;
+        return(other.red == this.red) && (other.blue == this.blue) && (other.green == this.green);
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
+    // Uncomment this method after you've written
+    //equals and failed the testHashCodeAndEqualsConsistency test.
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            // Write a perfect hash function for Simple Oomages.
+            int perfectCode = 0;
+            perfectCode += (red / 5);
+            perfectCode += (green / 5) * 53;
+            perfectCode += (blue / 5) * 53 * 71;
+
+            return perfectCode;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
